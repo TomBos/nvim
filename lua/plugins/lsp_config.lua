eaf24ce1957b0fecdba2a6c16d5eedfe525669cf
TomBos <tombos255@gmail.com>
2025-04-26 22:18:22 +0200

    Hotfix

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -18,7 +18,7 @@ return {
   {
     "neovim/lsp-config",
     config = function()
-      local lspconfig = require("lsp-config"),
+      local lspconfig = require("lsp-config")
       lspconfig.lua_ls.setup()
     end
   },
