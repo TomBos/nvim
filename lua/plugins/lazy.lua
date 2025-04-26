c5e7580a45b887b830830a7c2c7b8943b95f0c21
TomBos <tombos255@gmail.com>
2025-04-26 21:01:38 +0200

    onedark

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -19,12 +19,14 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
+    -- import plugins config file
     { import = "config" },
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { 
+    colorscheme = { 
+      "onedark" 
+    } 
+  },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
