c34370167bc1808292ed8dc286f72ac6547ea905
TomBos <tombos255@gmail.com>
2025-05-03 12:08:04 +0200

    More LSP servers

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -10,7 +10,14 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls"
+          "lua_ls",
+          "intelephense",
+          "jsonls",
+          "eslint",
+          "cssls",
+          "bashls",
+          "sqlls",
+          "lemminx"
         }
       })
     end
@@ -20,6 +27,13 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.intelephense.setup({})
+      lspconfig.jsonls.setup({})
+      lspconfig.eslint.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.bashls.setup({})
+      lspconfig.sqlls.setup({})
+      lspconfig.lemminx.setup({})
     end
   },
 }
